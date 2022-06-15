a2c9c56b46ea4abfc8a9f4376276807d5dbdac99
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-06-15 11:26:49 -0600

    init commit

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(public router: Router) { }
+
+  navigateHome() {
+    this.router.navigate(['/tabs/tab1']);
+  }
 
 }
