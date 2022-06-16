7b325f5a96480c70687f3cf2cc19762a5551b8a2
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-06-15 19:07:32 -0600

    home page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public router: Router) { }
 
+  navMenu() {
+    this.router.navigate(['/tabs/tab2']);
+  }
 }
