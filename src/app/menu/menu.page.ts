8a71153d157cdd4b203e8aa0a281c774765a4599
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-06-20 18:17:17 -0600

    Menu nav bar

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.page.scss'],
 })
 export class MenuPage implements OnInit {
-
+  menuTabs: any = ['Appetizers & Salads', 'Botanas Mexicanas', 'Burritos', 'Cocktails', 'Combination Plate', 'Chimichangas', 'Enchiladas', 'Quesadillas', 'Tortas', 'Quesadillas De Masa', 'Seafood', 'Fajitas', 'Quesadillas De Masa', 'Seafood', 'Fajitas']
+  activeTab: string = 'Appetizers & Salads';
   constructor() { }
 
   ngOnInit() {
   }
 
+  setActiveTab(btn) {
+    this.activeTab = btn;
+    console.log(btn);
+  }
 }
