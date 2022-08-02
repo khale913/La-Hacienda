f93643470d618a6b55b21abbb148e4f045c19938
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-08-01 17:21:27 -0700

    page celanup and contract page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { StorageService } from './servies/storage.service';
 import { AppRoutingModule } from './app-routing.module';
+import { MenuPage } from './menu/menu.page';
 import { AppComponent } from './app.component';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, LazyLoadImageModule, IonicModule.forRoot(), AppRoutingModule,],
+  declarations: [AppComponent,],
+  imports: [BrowserModule, LazyLoadImageModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [StorageService, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
