8b7d7101942e87f4fd9c881054508d321d4fe3a3
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-08-30 19:56:56 -0700

    changing image sizes

--- src/app/tab6/tab6.page.ts
+++ src/app/tab6/tab6.page.ts
@@ -6,11 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab6.page.scss'],
 })
 export class Tab6Page implements OnInit {
-  // image2 = '../../assets/images/tomatoes.jpg';
-  image2 = '../../assets/images/jarritos-mexican-soda-E0txIcqePac-unsplash.jpg';
-  // image2 = '../../assets/images/hybrid-storytellers-2M9-LTrpxJw-unsplash.jpg'
-  // image2 = '../../assets/images/peppers.jpg'
-  // image2 = '../../assets/images/brickpattern.jpg';
+  image2 = '../../assets/images/jarritos-mexican-soda-E0txIcqePac-unsplash (1).jpg';
   image1 = '../../assets/images/LaHaciendaFood.jpg';
   defaultImage1 = 'https://miro.medium.com/max/441/1*9EBHIOzhE1XfMYoKz1JcsQ.gif';
   defaultImage2 = 'https://miro.medium.com/max/441/1*9EBHIOzhE1XfMYoKz1JcsQ.gif';
